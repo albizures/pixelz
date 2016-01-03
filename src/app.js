7f56fa306173a696f82f6fb717750918676c52ff
albizures <albizures3601@gmail.com>
2016-01-03 09:23:55 -0600

    ...

--- src/app.js
+++ src/app.js
@@ -4,15 +4,6 @@ const express = require('express'),
       BUILD_PATH = path.resolve(__dirname,'..' ,'build');
 require("./server/config")(app);
 
-// app.get('/app.js', function(req, res) {
-//   res.sendFile(path.resolve(BUILD_PATH,'app.js'));
-// });
-//
-//
-// app.get('/style.css', function(req, res) {
-//   res.sendFile(path.resolve(BUILD_PATH,'style.css'));
-// });
-
 app.get('/', function(req, res) {
   res.sendFile(path.resolve(BUILD_PATH,'index.html'));
 });
