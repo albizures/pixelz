63d698e55af77d3ef4b093881482d9e0172eea49
albizures <albizures3601@gmail.com>
2016-01-05 21:06:57 -0600

    add progress build

--- src/app.js
+++ src/app.js
@@ -11,24 +11,20 @@ app.get('/', function(req, res) {
 if (!process.env.PRODUCTION) {
   var webpack = require('webpack');
   var compiler = webpack(require('./webpack.local.config'));
+	var ProgressPlugin = require('webpack/lib/ProgressPlugin');
+	compiler.apply(new ProgressPlugin(function(percentage, msg) {
+	  console.log((percentage * 100) + '%', msg);
+	}));
   compiler.watch({ // watch options:
     errorDetails : true,
     aggregateTimeout: 300, // wait so long for more changes
     poll: true // use polling instead of native watchers
     // pass a number to set the polling interval
   }, function(err, stats) {
-    console.log('termino');
+    console.log('ended');
     console.log(stats.toString({colors : true}));
-    /*if(err){
-    /}else{
-      var jsonStats = stats.toJson();
-      if(jsonStats.errors.length > 0)
-        for(item of jsonStats.errors){
-          console.log(item);
-        }
-      //console.log('termino');
-    }*/
   });
+
 }
 
 module.exports = app;
