b0fede0fdf01b6ef3043d1f1f02bfc4093e2c478
Jose Albizures <albizures3601@gmail.com>
2016-03-06 11:07:13 -0600

    performance improvements

--- src/client/prototypes/Vector.js
+++ src/client/prototypes/Vector.js
@@ -1,34 +1,40 @@
 'use strict';
+var diff, abs = Math.abs;
 function Vector(x, y) {
 	this.x = x || 0;
 	this.y = y || 0;
 }
 
-Vector.prototype.less = function (vector) {
-	if (arguments.length == 2) {
-		vector = new Vector(arguments[0], arguments[1]);
+Vector.prototype.less = function (x, y) {
+	if (arguments.length == 1) {
+		y = x.y;
+		x = x.x;
 	}
-	this.x -= vector.x;
-	this.y -= vector.y;
+	this.x -= x;
+	this.y -= y;
 	return this;
 };
-Vector.prototype.sum = function (vector) {
-	if (arguments.length == 2) {
-		vector = new Vector(arguments[0], arguments[1]);
+Vector.prototype.sum = function (x, y) {
+	if (arguments.length == 1) {
+		y = x.y;
+		x = x.x;
 	}
-	this.x += vector.x;
-	this.y += vector.y;
+	this.x += x;
+	this.y += y;
 	return this;
 };
 Vector.prototype.diffAbs = function (vector) {
-	return this.diff(vector, true);
-};
-Vector.prototype.diff = function (vector, abs) {
-	let x = this.x - vector.x,
-			y = this.y - vector.y;
 	return {
-		x : abs ? Math.abs(x) : x,
-		y : abs ? Math.abs(y) : y,
+		x : abs(this.x - vector.x),
+		y : abs(this.y - vector.y),
+		sx : (this.x < vector.x) ? 1 : -1,
+		sy : (this.y < vector.y) ? 1 : -1
+	};
+};
+Vector.prototype.diff = function (vector) {
+	return {
+		x : this.x - vector.x,
+		y : this.y - vector.y,
 		sx : (this.x < vector.x) ? 1 : -1,
 		sy : (this.y < vector.y) ? 1 : -1
 	};
@@ -37,7 +43,7 @@ Vector.prototype.clone = function () {
 	return new Vector(this.x, this.y);
 };
 Vector.prototype.importantDiff = function (vector) {
-	let diff = this.diff(vector, true);
+	diff = this.diffAbs(vector);
 	return diff.x > 1 || diff.y > 1;
 };
 module.exports = Vector;
