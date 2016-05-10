5d4f2ef77124b4958f4b43b60b13fd046a99697e
Jose Albizures <albizures3601@gmail.com>
2016-05-09 22:03:29 -0600

    add style to palette panel

--- src/client/panels/Palette/index.js
+++ src/client/panels/Palette/index.js
@@ -1,20 +1,20 @@
 'use strict';
 const {TRANSPARENT_COLOR, PALETTE} = require('../../constants/index.js'),
 	{CHANGE_COLOR} = require('../../constants/index.js').palette,
+	make = require('make'),
 	Panel = require('../../prototypes/Panel.js'),
 	{SNAP, FLOAT, B, L, R, TL, TR, BL, BR} = Panel,
 	Tools = require('../Tools.js'),
 	Vector = require('../../prototypes/Vector.js'),
 	Color = require('../../prototypes/Color.js'),
-	divColors = document.createElement('div'),
 	Palette = new Panel(PALETTE, SNAP, new Vector(100, 60), 15, 20, R);
 
-let time = 0.5 * 1000, loop, index = 0, ctx,
+let time = 0.5 * 1000, loop, index = 0, ctx, divColors,
 		colors = ['rgba(26, 188, 156, 1)', 'rgba(241, 196, 15, 1)', 'rgba(52, 152, 219, 1)', 'rgba(230, 126, 34, 1)', 'rgba(231, 76, 60, 1)', 'rgba(189, 195, 199, 1)', 'rgba(155, 89, 182, 1)', 'rgba(52, 73, 94, 1)', 'rgba(127, 140, 141, 1)'],
 		currentColors;
 
 Palette.mainInit = function () {
-	this.el.appendChild(divColors);
+	divColors = make(['div', {parent : this.el, className : 'content-colors'}]);
 	Tools.setPrimaryColor(colors[0]);
 	Tools.setSecudaryColor(TRANSPARENT_COLOR);
 	this.generateColors([]);
