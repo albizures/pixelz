0d0a84ead09d35b6d8d54a2d4c471bed47c4e5b1
Jose Albizures <albizures3601@gmail.com>
2016-06-12 18:09:28 -0600

    create sprites api

---
+++ src/server/api/sprites/sprites.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+const controller = require('./sprites.ctrl.js');
+
+router.get('/:id', controller.getOne);
+router.get('/', controller.getAll);
+router.get('/search', controller.getSearch);
+
+
+router.post('/', controller.post);
+
+module.exports = router;
---
+++ src/server/api/sprites/sprites.mdl.js
@@ -0,0 +1,18 @@
+const db = require('../../components/connect.js');
+const collection = 'sprites';
+
+exports.post = function (data, cb) {
+  db.post(collection, data, cb);
+};
+
+exports.getAll = function (cb) {
+  db.getAll(collection, cb);
+};
+
+exports.getOne = function (id, cb) {
+  db.getOne(collection, id, cb);
+};
+
+exports.getSearch = function (data, cb) {
+  db.getOne(collection, data, cb);
+};
