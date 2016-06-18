01858cba0abf35354a8ed298cfcaa00db4c2fab2
Jose Albizures <albizures3601@gmail.com>
2016-06-18 17:47:26 -0600

    valid status and parse json

--- src/client/utils/http.js
+++ src/client/utils/http.js
@@ -1,19 +1,27 @@
-function request(url, protocol, cb, body) {
-  if (body) {
+function parseJSON(response) {
+  return response.json();
+}
+function checkStatus(response) {
+  if (response.status >= 200 && response.status < 300) {
+    return response;
+  } else {
+    var error = new Error(response.statusText);
+    error.response = response;
+    throw error;
+  }
+}
+function request(url, protocol, cb, body, headers = true) {
+  if (body && !body instanceof FormData) {
     body = JSON.stringify(body);
   }
   fetch(url, {
-    headers: {
+    headers: headers? {
       'Accept': 'application/json',
       'Content-Type': 'application/json'
-    },
+    } : undefined,
     method: protocol,
     body: body
-  }).then(function (response) {
-    return response.json();
-  }).then(function (json) {
-    cb(json);
-  }).catch(function (ex) {
+  }).then(checkStatus).then(parseJSON).then(cb).catch(function (ex) {
     cb({code : 1, description : 'Connection error'});
   });
 }
@@ -35,7 +43,7 @@ exports.put = function (url, body, cb) {
   request(url, 'PUT', cb, body);
 };
 
-exports.upload = function (url, data, files) {
+exports.upload = function (url, data, files, cb) {
   const form = new FormData();
   const dataKeys = Object.keys(data);
   for (let j = 0; j < files.length; j++) {
@@ -46,13 +54,5 @@ exports.upload = function (url, data, files) {
   for (var key of form.values()) {
     console.log(typeof key);
   }
-
-  fetch(url, {
-    method: 'POST',
-    body: form
-  }).then(function (response) {
-    console.log(response);
-  }).catch(function (ex) {
-    cb({code : 1, description : 'Connection error'});
-  });
+  request(url, 'POST', cb, form, false);
 };
