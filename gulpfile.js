3fb778d4c52b2ce7f8f944232b7338cd7b40a896
albizures <albizures3601@gmail.com>
2016-08-14 07:24:43 -0600

    add open task

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 const path = require('path');
+var os = require('os');
 const gulp = require('gulp');
+const gutil = require('gulp-util');
+const open = require('gulp-open');
 const clean = require('gulp-clean');
 const gls = require('gulp-live-server');
 const webpack = require('webpack-stream');
+const config = require('./src/server/config/environment');
 var webpackConfig;
 
 gulp.task('set-dev', function () {
@@ -13,9 +17,54 @@ gulp.task('set-prod', function () {
   return process.env.NODE_ENV = 'production';
 });
 
-gulp.task('webpack', ['clean'], function () {
+let callingDone = false;
+let firstCompile = true; 
+gulp.task('webpack', ['clean'], function (cb) {
   return gulp.src('src/entry.js')
-    .pipe(webpack(webpackConfig))
+    .pipe(webpack(webpackConfig, undefined, function done(err, stats) {
+      if (err) {
+        // The err is here just to match the API but isnt used
+        return;
+      }
+      stats = stats || {};
+      if (callingDone) {
+        return;
+      }
+      // Debounce output a little for watch mode
+      callingDone = true;
+      setTimeout(function () {
+        callingDone = false;
+      }, 500);
+
+      // if (options.verbose) {
+      //   gutil.log(stats.toString({
+      //     colors: gutil.colors.supportsColor
+      //   }));
+      // } else {
+      var statsOptions = {
+        colors: gutil.colors.supportsColor,
+        hash: false,
+        timings: false,
+        chunks: false,
+        chunkModules: false,
+        modules: false,
+        children: true,
+        version: true,
+        cached: false,
+        cachedAssets: false,
+        reasons: false,
+        source: false,
+        errorDetails: false
+      };
+
+      gutil.log(stats.toString(statsOptions));
+      // }
+      if (firstCompile) {
+        console.log('init compile');
+        cb();
+        firstCompile = false;
+      }
+    }))
     .pipe(gulp.dest(webpackConfig.output.path));
 });
 
@@ -25,7 +74,6 @@ gulp.task('server', function () {
   server.start();
 
   gulp.watch([path.join(webpackConfig.output.path, '/**/*.{html,css,js}')], function (file) {
-    console.log('change');
     server.notify.apply(server, [file]);
   });
 
@@ -40,6 +88,17 @@ gulp.task('clean', function (cb) {
     .pipe(clean());
 });
 
-gulp.task('default', ['set-dev', 'webpack', 'server']);
+gulp.task('default', ['set-dev', 'webpack', 'server'], function () {
+  var browser = os.platform() === 'linux' ? 'google-chrome' : (
+    os.platform() === 'darwin' ? 'google chrome' : 'chrome');
+  var options = {
+    uri: 'http://localhost:' + config.PORT,
+    app: browser
+  };
+  return gulp.src(__filename)
+    .pipe(open(options));
+});
 
 gulp.task('build', ['set-prod', 'webpack']);
+
+
