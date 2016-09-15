53269e35ae35f8a86be25375c6329f294dd9c978
albizures <albizures3601@gmail.com>
2016-09-14 18:40:25 -0600

    change environment config

--- src/server/config/environment/index.js
+++ src/server/config/environment/index.js
@@ -2,38 +2,30 @@
 const path = require('path');
 const _ = require('lodash');
 
-const env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-const ROOT_PATH = path.join(__dirname, '..', '..', '..', '..');
-const FILES_PATH = path.join(ROOT_PATH, 'files');
-const PUBLIC_PATH = path.join(ROOT_PATH, 'public');
-const CLIENT_PATH = path.join(ROOT_PATH, 'src', 'client');
-const APP_PATH = path.join(CLIENT_PATH, 'init.js');
-const MODULES_PATH = path.join(ROOT_PATH, 'node_modules');
-const ASSETS_PATH = path.join(CLIENT_PATH, 'assets');
-const TEMPLATE_PATH = path.join(CLIENT_PATH, 'template');
-const all = {
-  isDev : env === 'development',
-  isProduction: env != 'development',
-  isProd : env != 'development',
-  ROOT_PATH,
-  PUBLIC_PATH,
-  CLIENT_PATH,
-  APP_PATH,
-  MODULES_PATH,
-  TEMPLATE_PATH,
-  ASSETS_PATH,
-  FILES_PATH,
-  PORT : 8080,
-};
+const config = {};
 
-all.auth = {
+config.PORT = 8000;
+
+config.isDev = process.env.NODE_ENV === 'development';
+config.ROOT_PATH = path.join(__dirname, '..', '..', '..', '..');
+config.FILES_PATH = path.join(config.ROOT_PATH, 'files');
+config.PUBLIC_PATH = path.join(config.ROOT_PATH, 'public');
+config.CLIENT_PATH = path.join(config.ROOT_PATH, 'src', 'client');
+config.APP_PATH = path.join(config.CLIENT_PATH, 'init.js');
+config.MODULES_PATH = path.join(config.ROOT_PATH, 'node_modules');
+config.ASSETS_PATH = path.join(config.CLIENT_PATH, 'assets');
+config.TEMPLATE_PATH = path.join(config.CLIENT_PATH, 'template');
+config.MAIN_TEMPLATE = path.join(config.TEMPLATE_PATH, 'production.jade');
+config.auth = {
   twitter:{
     TWITTER_CONSUMER_KEY : process.env.TWITTER_CONSUMER_KEY,
     TWITTER_CONSUMER_SECRET: process.env.TWITTER_CONSUMER_SECRET,
-    callbackURL: 'http://pixore.test' 
+    callbackURL: 'http://pixore.test'
   }
 };
-module.exports = _.merge(
-  all,
-  require('./' + env + '.js')(all)
-);
+
+if (config.isDev) {
+  config.MAIN_TEMPLATE = path.join(config.TEMPLATE_PATH, 'development.jade');
+}
+
+module.exports = config;
--- src/server/config/environment/production.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-
-module.exports = function (config) {
-  return {
-    MAIN_TEMPLATE : path.join(config.TEMPLATE_PATH, 'production.jade')
-  };
-};
